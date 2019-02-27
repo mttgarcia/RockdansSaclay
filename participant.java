8f96dbfa5efc7cf134854e10cc5607fd4631ddc1
ydgaygui <48050438+ydgaygui@users.noreply.github.com>
2019-02-27 14:58:03 +0100

    Add files via upload
    Ajout de la fonction pour payer et recevoir, système de compteur pour le numéro de participant.

--- participant.java
+++ participant.java
@@ -7,27 +7,36 @@ import javacard.framework.ISOException;
 
 public class Participant extends Applet {
 
+        //Numéro de mon applet
         public static final byte CLA_MONAPPLET = (byte) 0xB0;
 
+        //Les get
         public static final byte INS_GET_NOM = 0x00;
         public static final byte INS_GET_PRENOM = 0x01;
         public static final byte INS_GET_CREDIT = 0x02;
         public static final byte INS_GET_NUMPARTICIPANT = 0x03;
 
+        //Les initialisations
         public static final byte INS_SET_NOM = 0x10;
         public static final byte INS_SET_PRENOM = 0x20;
         public static final byte INS_SET_NUMPARTICIPANT = 0x30;
 
+        //Payer et décrémenter le crédit
+        public static final byte INS_PAYER = 0xA0;
 
+        //Les tailles
         private static final byte NOM_LENGHT = 0x10;
         private static final byte NUMPARTICIPANT_LENGHT = 0x08;
+
+        //Exception
+        private static final byte SOLDE_INSUFISANT = 0x6302;
 
         /* Attributs */
         private byte credit;
         private byte [] nom;
         private byte [] prenom;
         private byte [] numparticipant;
-
+        private static final byte compteur = 1;
 
         /* Constructeur */
         private Participant() {
@@ -35,6 +44,8 @@ public class Participant extends Applet {
                nom = new byte[(short)NOM_LENGHT];
                prenom = new byte[(short)NOM_LENGHT];
                numparticipant = new byte[(short)NUMPARTICIPANT_LENGHT];
+               numparticipant = compteur;
+               compteur ++;
         }
 
         public static void install(byte bArray[], short bOffset, byte bLength) throws ISOException {
@@ -53,12 +64,12 @@ public void process(APDU apdu) throws ISOException {
 
     switch (buffer[ISO7816.OFFSET_INS]) {
             case INS_GET_NOM:
-                    buffer[0] = nom;
+                    Util.arrayCopy(nom, ISO7816.OFFSET_CDATA, buffer, (byte)0, ISO7816.OFFSET_LC); //Prendre ce qu'il y a dans nom, et le mettre dans le buffer
                     apdu.setOutgoingAndSend((short) 0, (short) len(nom));
                     break;
 
             case INS_GET_PRENOM:
-                    buffer[0] = prenom;
+                    Util.arrayCopy(prenom, ISO7816.OFFSET_CDATA, buffer, (byte)0, ISO7816.OFFSET_LC); //Prendre ce qu'il y a dans nom, et le mettre dans le buffer
                     apdu.setOutgoingAndSend((short) 0, (short) len(prenom));;
                     break;
 
@@ -68,7 +79,7 @@ public void process(APDU apdu) throws ISOException {
                     break;
 
             case INS_GET_NUMPARTICIPANT:
-                    buffer[0] = numparticipant;
+                    Util.arrayCopy(numparticipant, ISO7816.OFFSET_CDATA, buffer, (byte)0, ISO7816.OFFSET_LC);
                     apdu.setOutgoingAndSend((short) 0, (short) len(numparticipant));
                     break;
 
@@ -86,7 +97,30 @@ public void process(APDU apdu) throws ISOException {
                     apdu.setIncomingAndReceive();
                     Util.arrayCopy(buffer, ISO7816.OFFSET_CDATA, numparticipant, (byte)0, ISO7816.OFFSET_LC);
                     break;
+
+            case INS_PAYER:
+                apdu.setIncomingAndReceive();
+                payer(buffer[ISO7816.OFFSET_CDATA]);
+
+                break;
+
             default:
                     ISOException.throwIt(ISO7816.SW_INS_NOT_SUPPORTED);
     }
+}
+
+//Fonction pour payer et décrémenter le crédit
+public void payer(byte prix) throws ISOException{
+    if (credit >= prix) {
+        credit = credit - prix;
+        return;
+    }
+    ISOException.throwIt(SOLDE_INSUFISANT);
+    }
+
+//Fonction pour recevoir et incrémenter le crédit
+public void recevoir(byte gain) throws ISOException{
+    credit = credit + gain;
+    return;
+
 }
