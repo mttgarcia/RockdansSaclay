23ce8c00d26c9dc7c3d84a86dc26caf9ed9b50ff
ydgaygui <48050438+ydgaygui@users.noreply.github.com>
2019-02-28 15:03:52 +0100

    Add files via upload

--- Participant/src/part/Participant.java
+++ Participant/src/part/Participant.java
@@ -1,55 +1,77 @@
-package part;
+    package part;
 
-import javacard.framework.APDU;
-import javacard.framework.Applet;
-import javacard.framework.ISO7816;
-import javacard.framework.ISOException;
-import javacard.framework.Util;
+    import javacard.framework.APDU;
+    import javacard.framework.Applet;
+    import javacard.framework.ISO7816;
+    import javacard.framework.ISOException;
+    import javacard.framework.Util;
+    import javacard.framework.OwnerPIN;
 
-public class Participant extends Applet {
+    public class Participant extends Applet {
 
-        //Numéro de mon applet
         public static final byte CLA_MONAPPLET = (byte) 0xB0;
-
-        //Les get
-        public static final byte INS_GET_NOM = 0x00;
-        public static final byte INS_GET_PRENOM = 0x01;
-        public static final byte INS_GET_CREDIT = 0x02;
-        public static final byte INS_GET_NUMPARTICIPANT = 0x03;
-
-        //Les initialisations
-        public static final byte INS_SET_NOM = 0x04;
-        public static final byte INS_SET_PRENOM = 0x05;
-
-        //Payer et décrémenter le crédit
-        public static final byte INS_PAYER = 0x06;
-
-        //Les tailles
-        private static final byte NOM_LENGHT = 0x10;
-
+        //Instruction
+        static final byte INS_PAYER = 0x01;
+        static final byte INS_AFFICHER_CREDIT = 0x02;
+        static final byte INS_RECEVOIR = 0x03;
         //Exception
-        private static final short SOLDE_INSUFISANT = 0x6302;
+        public static final short SW_SOLDE_INSSUFISANT = 0x6301;
+        static final short SW_PIN_VERIFICATION_REQUIRED = 0x6302;
+        static final short SW_PIN_VERIFICATION_FAILED = 0x6303;
+        //Taille
+        private static final byte PIN_LENGTH = 0x02;
+        private static final byte NOM_LENGTH = 0x0c;
+        private static final byte PRENOM_LENGTH = 0x0c;
+        private static final byte NUM_PARTICIPANT_LENGTH = 0x05;
+        //PIN
+        private static final byte PIN_TRY_LIMIT = 0x03;
 
-        /* Attributs */
+        //Atribut
         private byte credit;
-        private byte [] nom;
-        private byte [] prenom;
-        private byte numparticipant;
-        private static byte compteur = 1;
+        private byte[] nom;
+        private byte[] prenom;
+        private OwnerPIN pin;
 
-        /* Constructeur */
-        private Participant() {
-               credit = 50;
-               nom = new byte[(short)NOM_LENGHT];
-               prenom = new byte[(short)NOM_LENGHT];
-               numparticipant = compteur;
-               compteur ++;
+        private Participant(byte bArray[], short bOffset, byte bLength) {
+            //Taille de l'aid
+            byte aidLength = bArray[bOffset];
+            //Control info (on s'en fout c'est 0 faut le mettre pour faire joli et propre)
+            short controlLength = (short)(bArray[(short)(bOffset+1+aidLength)]&(short)0x00FF);
+            //Taille des info 
+            short dataLength = (short)(bArray[(short)(bOffset+1+aidLength+1+controlLength)]&(short)0x00FF);
+            if ((byte)dataLength != (byte)(NUM_PARTICIPANT_LENGTH + PRENOM_LENGTH + NOM_LENGTH + PIN_LENGTH)) {
+                ISOException.throwIt(ISO7816.SW_WRONG_LENGTH );
+            } 
+
+            //Initialisation du PIN
+            pin = new OwnerPIN(PIN_TRY_LIMIT, //Max d'essais de PIN
+                                    PIN_LENGTH);//Taille du PIN
+            pin.update(bArray,//Tab
+                    (short) (bOffset+1+aidLength+1+controlLength+1),//Offset
+                    PIN_LENGTH);//Taille du pin
+
+            //Initialisation du nom
+            nom = new byte[(short)NOM_LENGTH];
+            Util.arrayCopyNonAtomic(bArray,//source
+                                        (short) (bOffset+1+aidLength+1+controlLength+1+PIN_LENGTH),//offset de source
+                                        nom,//dest
+                                        (short)0,//offset de dest
+                                        NOM_LENGTH);//Taille de la copie
+
+            //Initialisation du prenom
+            prenom = new byte[(short)NOM_LENGTH];
+            Util.arrayCopyNonAtomic(bArray,//source
+                                        (short) (bOffset+1+aidLength+1+controlLength+1+PIN_LENGTH+NOM_LENGTH),//offset de source
+                                        prenom,//dest
+                                        (short)0,//offset de dest
+                                        PRENOM_LENGTH);//Taille de la copie
+
+            credit = 50;
         }
 
         public static void install(byte bArray[], short bOffset, byte bLength) throws ISOException {
-               new Participant().register();
+            new Participant(bArray,bOffset,bLength).register();
         }
-
 
         public void process(APDU apdu) throws ISOException {
             byte[] buffer = apdu.getBuffer();
@@ -57,63 +79,29 @@ public class Participant extends Applet {
             if (this.selectingApplet()) return;
 
             if (buffer[ISO7816.OFFSET_CLA] != CLA_MONAPPLET) {
-                    ISOException.throwIt(ISO7816.SW_CLA_NOT_SUPPORTED);
+                ISOException.throwIt(ISO7816.SW_CLA_NOT_SUPPORTED);
             }
 
             switch (buffer[ISO7816.OFFSET_INS]) {
-                    case INS_GET_NOM:
-                            Util.arrayCopy(nom, ISO7816.OFFSET_CDATA, buffer, (byte)0, ISO7816.OFFSET_LC); //Prendre ce qu'il y a dans nom, et le mettre dans le buffer
-                            apdu.setOutgoingAndSend((short) 0, (short) nom.length);
-                            break;
 
-                    case INS_GET_PRENOM:
-                            Util.arrayCopy(prenom, ISO7816.OFFSET_CDATA, buffer, (byte)0, ISO7816.OFFSET_LC); //Prendre ce qu'il y a dans nom, et le mettre dans le buffer
-                            apdu.setOutgoingAndSend((short) 0, (short) prenom.length);;
-                            break;
+                case INS_AFFICHER_CREDIT:
+                    buffer[0] = credit;
+                    apdu.setOutgoingAndSend((short) 0, (short) 1);
+                    break;
 
-                    case INS_GET_CREDIT:
-                            buffer[0] = credit;
-                            apdu.setOutgoingAndSend((short) 0, (short) 1);
-                            break;
+                case INS_PAYER:
+                    if(buffer[0] > credit) payer(apdu);
+                    break;
 
-                    case INS_GET_NUMPARTICIPANT:
-                            buffer[0] = numparticipant;
-                            apdu.setOutgoingAndSend((short) 0, (short) 1);
-                            break;
-
-                     case INS_SET_NOM:
-                            apdu.setIncomingAndReceive();
-                            Util.arrayCopy(buffer, ISO7816.OFFSET_CDATA, nom, (byte)0, ISO7816.OFFSET_LC);
-                            break;
-
-                    case INS_SET_PRENOM:
-                            apdu.setIncomingAndReceive();
-                            Util.arrayCopy(buffer, ISO7816.OFFSET_CDATA, prenom, (byte)0, ISO7816.OFFSET_LC);
-                            break;
-
-                    case INS_PAYER:
-                        apdu.setIncomingAndReceive();
-                        payer(buffer[ISO7816.OFFSET_CDATA]);
-
-                        break;
-
-                    default:
-                            ISOException.throwIt(ISO7816.SW_INS_NOT_SUPPORTED);
+                default:
+                        ISOException.throwIt(ISO7816.SW_INS_NOT_SUPPORTED);
             }
         }
 
-        //Fonction pour payer et décrémenter le crédit
-        public void payer(short prix) throws ISOException{
-            if (credit >= prix) {
-                credit -= prix;
-                return;
-            }
-            ISOException.throwIt(SOLDE_INSUFISANT);
-        }
-
-        //Fonction pour recevoir et incrémenter le crédit
-        public void recevoir(short gain) throws ISOException{
-            credit += gain;
+        public void payer(APDU apdu){
+            byte[] buffer = apdu.getBuffer();
+            apdu.setIncomingAndReceive(); //Récupere le buffer
+            credit -= buffer[0];
             return;
         }
-}
+    }
