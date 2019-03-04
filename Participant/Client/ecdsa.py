7ee077e68edc13b61efab229ec071bb988ba3e5c
ydgaygui <48050438+ydgaygui@users.noreply.github.com>
2019-03-04 12:32:31 +0100

    Delete ecdsa.py

--- Participant/Client/ecdsa.py
+++
@@ -1,5 +0,0 @@
-from ecdsa import SigningKey, NIST224p
-sk = SigningKey.generate(curve = NIST224p)
-vk = sk.get_verifying_key()
-open("private.pem","w").write(sk.to_pem().decode())
-open("public.pem","w").write(vk.to_pem().decode())
